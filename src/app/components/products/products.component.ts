11b728200619ea2ec0dfeb7a622a0d00cce0a2bc
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2021-10-15 04:05:01 +0100

    injecting products service and getting all products in table

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/model/product/product.model';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
+  products?:Product[]; //ou |null=null
+  constructor(private productsService:ProductsService) { }
 
   ngOnInit(): void {
   }
-
+  getProducts(){
+    return this.productsService.getProducts().subscribe(data=>
+      {this.products = data;})
+  }
 }
