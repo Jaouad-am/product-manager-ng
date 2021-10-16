89c7e2fc8f04fce691c42d432e85660f592889a4
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2021-10-16 02:22:35 +0100

    using rxjs to get products with pipe(map, startWith, catchError operators)

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from 'src/app/model/product/product.model';
 import { ProductsService } from 'src/app/services/products.service';
+import { catchError, map, startWith } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-products',
@@ -8,13 +11,25 @@ import { ProductsService } from 'src/app/services/products.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products?:Product[]; //ou |null=null
+  //products?:Product[]; //ou |null=null
+  products$:Observable<Product[]> |null=null;
+
   constructor(private productsService:ProductsService) { }
 
   ngOnInit(): void {
   }
   getProducts(){
-    return this.productsService.getProducts().subscribe(data=>
-      {this.products = data;})
+   /* return this.productsService.getProducts().subscribe(data=>
+      {this.products = data;}, err=>{console.log(err.message);})*/
+
+    this.products$ = this.productsService.getProducts().pipe(
+      map(data=>({dataState: "LOADED", data:data})),
+      startWith({dataState: "LOADING"}),
+      catchError(err=>of({dataState: "LOADED", errorMessage: err.message}))
+      
+      );
+    
+    ;
+
   }
 }
