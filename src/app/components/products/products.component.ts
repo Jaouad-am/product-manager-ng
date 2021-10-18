0a1b66e589c977aa61caa279415a18b7eddc955c
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2021-10-18 01:16:55 +0100

    adding getSelectedProducts & getAvailableProducts methods onclick

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -13,23 +13,41 @@ import { AppDataState, DataStateEnum } from 'src/app/state/product.state';
 })
 export class ProductsComponent implements OnInit {
   //products?:Product[]; //ou |null=null
-  products$:Observable<AppDataState<Product[]>> |null=null;
-  readonly DataStateEnum=DataStateEnum;
+  products$: Observable<AppDataState<Product[]>> | null = null;
+  readonly DataStateEnum = DataStateEnum;
 
-  constructor(private productsService:ProductsService) { }
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
   }
-  getProducts(){
-   /* return this.productsService.getProducts().subscribe(data=>
-      {this.products = data;}, err=>{console.log(err.message);})*/
+  getProducts() {
+    /* return this.productsService.getProducts().subscribe(data=>
+       {this.products = data;}, err=>{console.log(err.message);})*/
 
     this.products$ = this.productsService.getProducts().pipe(
-      map(data=>({dataState: DataStateEnum.LOADED, data:data})),
-      startWith({dataState: DataStateEnum.LOADING}),
-      catchError(err=>of({dataState: DataStateEnum.ERROR, errorMessage: err.message}))
-      
-      );
+      map(data => ({ dataState: DataStateEnum.LOADED, data: data })),
+      startWith({ dataState: DataStateEnum.LOADING }),
+      catchError(err => of({ dataState: DataStateEnum.ERROR, errorMessage: err.message }))
 
+    );
+  }
+
+  getSelectedProducts() {
+
+    this.products$ = this.productsService.getSelectedProducts().pipe(
+      map(data => ({ dataState: DataStateEnum.LOADED, data: data })),
+      startWith({ dataState: DataStateEnum.LOADING }),
+      catchError(err => of({ dataState: DataStateEnum.ERROR, errorMessage: err.message }))
+
+    );
+  }
+  getAvailableProducts() {
+
+    this.products$ = this.productsService.getAvailableProducts().pipe(
+      map(data => ({ dataState: DataStateEnum.LOADED, data: data })),
+      startWith({ dataState: DataStateEnum.LOADING }),
+      catchError(err => of({ dataState: DataStateEnum.ERROR, errorMessage: err.message }))
+
+    );
   }
 }
