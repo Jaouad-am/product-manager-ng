28c95493a75b68950b4e6909778b790d330d2545
Jaouad-am <32226336+Jaouad-am@users.noreply.github.com>
2021-11-11 02:56:47 +0100

    using subject(publishevent) in product-item component

--- src/app/components/products/products-list/product-item/product-item.component.ts
+++ src/app/components/products/products-list/product-item/product-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Product } from 'src/app/model/product/product.model';
+import { EventDriverService } from 'src/app/state/event.driver.service';
 import { ActionEvent, productActionsTypes } from 'src/app/state/product.state';
 
 @Component({
@@ -10,23 +11,26 @@ import { ActionEvent, productActionsTypes } from 'src/app/state/product.state';
 export class ProductItemComponent implements OnInit {
 
   @Input() product: Product | null = null;
-  @Output() productEventEmitter: EventEmitter<ActionEvent> = new EventEmitter<ActionEvent>();
+  //@Output() productEventEmitter: EventEmitter<ActionEvent> = new EventEmitter<ActionEvent>();
 
-  constructor() { }
+  constructor(private eventDrivenService: EventDriverService) { }
 
   ngOnInit(): void {
   }
 
   selectProduct(product: Product) {
-    this.productEventEmitter.emit({ type: productActionsTypes.SELECT_PRODUCT, payload: product })
+    //this.productEventEmitter.emit({ type: productActionsTypes.SELECT_PRODUCT, payload: product })
+    this.eventDrivenService.publishEvent({ type: productActionsTypes.SELECT_PRODUCT, payload: product });
   }
 
   deleteProduct(product: Product) {
-    this.productEventEmitter.emit({ type: productActionsTypes.DELETE_PRODUCT, payload: product })
+    //this.productEventEmitter.emit({ type: productActionsTypes.DELETE_PRODUCT, payload: product })
+    this.eventDrivenService.publishEvent({ type: productActionsTypes.DELETE_PRODUCT, payload: product });
   }
 
   onUpdate(product: Product) {
-    this.productEventEmitter.emit({ type: productActionsTypes.UPDATE_PRODUCT, payload: product })
+    //this.productEventEmitter.emit({ type: productActionsTypes.UPDATE_PRODUCT, payload: product })
+    this.eventDrivenService.publishEvent({ type: productActionsTypes.UPDATE_PRODUCT, payload: product });
   }
 
 }
